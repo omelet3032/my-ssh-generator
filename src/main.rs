50cc927e142d33c778b83de0eaf3e4e938bbf1af
omelet1 <omelet1@omelet1ui-MacBook-Neo.local>
2026-08-16 21:00:48 +0900

    siging_key값 조회

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use getrandom::{SysRng, rand_core::UnwrapErr};
+use rand_core::TryRng;
+use ed25519_dalek::SigningKey;
+use ed25519_dalek::Signature;
 fn main() {
-    println!("Hello, world!");
+    let mut csprng = UnwrapErr(SysRng);
+
+    let signing_key = SigningKey::generate(&mut csprng);
+
+    println!("{:?}:", signing_key);
 }
